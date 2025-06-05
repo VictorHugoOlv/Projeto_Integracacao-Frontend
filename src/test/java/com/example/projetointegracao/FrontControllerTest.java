a0ce5fd15dc6e60125bab450667377076676adf9
Victor Hugo <victor.hugo.olv@gmail.com>
2025-06-05 15:24:30 -0300

    Criação: Teste da Linha Seleciona concluído

--- src/test/java/com/example/projetointegracao/FrontControllerTest.java
+++ src/test/java/com/example/projetointegracao/FrontControllerTest.java
@@ -1,11 +1,15 @@
 package com.example.projetointegracao;
 
 import com.example.projetointegracao.controllers.FrontController;
+import com.example.projetointegracao.dto.CategoryDTO;
 import com.example.projetointegracao.dto.LineDTO;
+import com.example.projetointegracao.dto.ProductDTO;
 import com.example.projetointegracao.services.CategoryService;
 import com.example.projetointegracao.services.LineService;
+import javafx.collections.FXCollections;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -46,7 +50,7 @@ public class FrontControllerTest {
 
     @Test
     @DisplayName("Garantir que a ComboBox está sendo preenchida corretamente")
-    void InitializeTest() {
+    void initializeTest() {
         List<LineDTO> lineDTOList = Arrays.asList(new LineDTO(1L, "linha1"));
         when(lineService.getAllLines()).thenReturn(lineDTOList);
 
@@ -54,5 +58,28 @@ public class FrontControllerTest {
 
         assertEquals(1, frontController.lineComboBox.getItems().size());
         assertEquals("linha1", frontController.lineComboBox.getItems().get(0).getName());
+    }
+
+    @Test
+    @DisplayName("Garantir que a linha selecionada retorne seus respectivos componentes")
+    void onItemSelectedLineComboTest(){
+        LineDTO selectedLine = new LineDTO(1L, "linha1");
+        frontController.lineComboBox.setItems(FXCollections.observableArrayList(selectedLine));
+        frontController.lineComboBox.getSelectionModel().select(selectedLine);
+
+        List<ProductDTO> products = Arrays.asList(new ProductDTO(100L,"produto1",10L));
+        CategoryDTO category = new CategoryDTO(10L, "categoria1", 1L, products);
+
+        when(categoryService.getCategoriesByLine(1L)).thenReturn(Arrays.asList(category));
+
+        frontController.onItemSelectedLineCombo();
+
+        assertFalse(frontController.modelsTitledPane.isDisabled());
+        assertTrue(frontController.modelsTitledPane.isExpanded());
+
+        TreeItem<String> root = frontController.categoriesWithProductsTreeView.getRoot();
+        assertEquals("Categorias com Produtos", root.getValue());
+        assertEquals("categoria1", root.getChildren().get(0).getValue());
+        assertEquals("produto1", root.getChildren().get(0).getChildren().get(0).getValue());
     }
 }
